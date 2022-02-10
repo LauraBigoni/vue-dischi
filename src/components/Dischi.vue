1a7bfaef5bfb97043ee8bc5cf6d2d1426668698b
Laura Bigoni <laurabigoni.92@gmail.com>
2022-02-10 17:02:03 +0100

    added style

--- src/components/Dischi.vue
+++ src/components/Dischi.vue
@@ -2,9 +2,12 @@
 	<div class="container dischi">
 		<div class="row text-center">
 			<div class="col">
-				<ul class="list-unstyled">
+				<ul class="list-unstyled d-flex flex-wrap justify-content-around align-items-start">
 					<li v-for="cd in music" :key="cd.author">
 						<img class="img-fluid" :src="cd.poster" :alt="cd.title" />
+						<p class="h5">{{ cd.title }}</p>
+						<p class="m-0">{{ cd.author }}</p>
+						<span>{{ cd.year }}</span>
 					</li>
 				</ul>
 			</div>
@@ -39,10 +42,23 @@ export default {
 
 <style scoped lang="scss">
 .dischi {
-	font-weight: 700;
+    ul {
+        padding-top: 30px;
+    }
+	li {
+		width: 220px;
+        height: 300px;
+        padding: 20px 0;
+        margin-bottom: 15px;
+        background-color: rgba(255,255,255,0.2);
+	}
 	img {
 		width: 150px;
 		height: auto;
+	}
+	p {
+		text-transform: uppercase;
+		font-weight: 700;
 	}
 }
 </style>
